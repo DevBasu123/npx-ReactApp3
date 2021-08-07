bbda0a3ec512930c619450ba4d7d9b91d3030203
Chandan Basumatary <cbasumat@in.ibm.com>
2021-08-07 20:01:51 +0530

    Fix Issue in Fetching Meals

--- react-food-order/src/components/Meals/AvailableMeals.js
+++ react-food-order/src/components/Meals/AvailableMeals.js
@@ -8,7 +8,7 @@ const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
 
     useEffect( () => {
-      const URL = `https://${process.env.DB_NAME}.${process.env.DB_LOCATION}/${process.env.DB_COLLECTION}`;
+      const URL = `${process.env.DB_NAME}/${process.env.DB_COLLECTION}`;
       const fetchMeals = async () => {
         const response = await fetch(URL);
         const responseData = await response.json();
